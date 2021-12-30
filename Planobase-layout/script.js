a65f94a991d8736e16bfae5b75b1077be5982abd
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2021-12-30 17:18:11 -0300

    t

--- Planobase-layout/script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    let xx = document.getElementById("myLinks");
-    if (xx.style.display === "block") {
-        xx.style.display = "none"
-    } else {
-        xx.style.display = "block"
-    }
-}
