c5cf293074ea7f8faa3804a7a8d047d187034aef
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2021-12-30 16:12:31 -0300

    e

---
+++ Navmenu responsivo/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    let xx = document.getElementById("myLinks");
+    if (xx.style.display === "block") {
+        xx.style.display = "none"
+    } else {
+        xx.style.display = "block"
+    }
+}
