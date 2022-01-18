cd96761fc3ed341b9255b98c83ffa90252419dcb
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-18 20:27:08 -0300

    a

--- Tiger school/cabecalho.js
+++ Tiger school/cabecalho.js
@@ -3,9 +3,12 @@ const menuNav = document.querySelector(".navbar");
 const tituloBackMobile = document.querySelector(".titulo-backmobile")
 
 botao.addEventListener("click", function botaonavmenu() {
-    menuNav.classList.toggle('navbar--ativo')
+    menuNav.classList.toggle('navbar--ativo');
+    // tituloBackMobile.style.top = "130px";
+
 
 })
+
 
 //setTimeout(() => function botaonavmenu() , 3000)
 
