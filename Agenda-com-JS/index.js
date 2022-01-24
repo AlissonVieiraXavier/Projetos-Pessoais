631d7d6ecb47ef4393e85d747c65d64bd6a544dc
Alisson Vieira Xavier <alissonvieiraxavier@hotmail.com>
2022-01-24 19:24:15 -0300

    a

--- Agenda-com-JS/index.js
+++ Agenda-com-JS/index.js
@@ -1,13 +1,17 @@
 const bottom = document.querySelector("#bottom");
 const tarefa = document.querySelector("#tarefa");
-const horario = document.querySelector("#tarefa-horario")
-const resultado = document.querySelector(".resultado")
+const horario = document.querySelector("#tarefa-horario");
+const resultado = document.querySelector(".resultado");
+resultado.style.display = "none"
 
 bottom.addEventListener("click", () => {
 
     if (horario.value == "" || horario.value == "") {
         alert("Digite algo antes de anexar a agenda")
     } else {
+        resultado.style.display = "flex";
+        resultado.style.flexDirection = "column";
+
         let elemento = document.createElement("p");
         elemento.textContent = `• ás ${horario.value} tenho que ${tarefa.value}.`
         resultado.appendChild(elemento);
