abc4753e44f5ac83e6a51ffbe619f700c0ffafef
Alisson Xavier <78052229+AlissonVieiraXavier@users.noreply.github.com>
2022-06-27 10:31:45 -0300

    adição do carousel com parceiros

--- Projetos-Portifolio/Concessionaria/js/main.js
+++ Projetos-Portifolio/Concessionaria/js/main.js
@@ -1,7 +1,7 @@
 (function($) { 
     $(function() { 
 
-         /* ///////////////////// NAVBAR TOP- START //////////////////// */
+         /* ///////////////////// Icons - START //////////////////// */
          $(".youtubeicon").click( () => {
            window.open("https://www.youtube.com/","_blank")
          })
@@ -19,7 +19,7 @@
         })
          
          
-         /* ////////////////// NAVBAR END - END ///////////////////////*/
+         /* ////////////////// icons - END ///////////////////////*/
          /* ///////////////////// NAVBAR - START //////////////////// */
   
       //  open and close nav 
@@ -45,8 +45,41 @@
       $('html').click(function() {
         $('.navbar-dropdown').hide();
       });
+      /* ////////////////// NAVBAR - END ///////////////////////*/
+      /* ////////////////// PARTNERS ///////////////////////*/
+      $('.autoplay-parceiros').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows:false,
+        responsive:[
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+      /* ////////////////// PARTNERS - END ///////////////////////*/
     }); 
-    /* ////////////////// NAVBAR - END ///////////////////////*/
-     /* ////////////////// BANNER-TOP ///////////////////////*/
   })(jQuery); 
 
